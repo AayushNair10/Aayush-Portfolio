e81728e8de2aaa80c0dfb0785a7e604ae9ae1979
AayushNair10 <aayushnair204@gmail.com>
2025-11-27 17:04:50 -0500

    footer error resolve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
